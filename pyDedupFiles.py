8aa8b5879acf71bebd1cc6c3b4bc349f66977764
rarsa <rarsa@yahoo.com>
2013-12-28 13:08:19 -0500

    Allow re-running the visual comparison skipping files that have already been deleted

--- pyDedupFiles.py
+++ pyDedupFiles.py
@@ -22,6 +22,8 @@ class Application(Tkinter.Frame):
     del_left_btn = None
     keep_both_btn = None
     del_right_btn = None
+    left_set = False
+    right_set = False
 
     
     def __init__(self, master):
@@ -29,7 +31,7 @@ class Application(Tkinter.Frame):
         self.file_handler = FileHandler("duplicates.txt")
         self.grid()
         self.create_widgets()
-        self.initialize_images()
+        self.next_image()
 
     def create_widgets(self):
         self.left_img_label = Tkinter.Label(self, relief=Tkinter.SUNKEN, justify=Tkinter.LEFT, padx=10, pady=10, text="Left side image ")
@@ -49,75 +51,74 @@ class Application(Tkinter.Frame):
         self.status_label = Tkinter.Label(self, justify=Tkinter.LEFT, padx=10, pady=10, text="Welcome to Rarsa's deduplicate")
         self.status_label.grid(row=3, column=1, columnspan=5)
 
-    def initialize_images(self):
-        image_path = self.file_handler.read_next().split("  ")[1]
-        self.set_left_side_image(image_path)
-        image_path = self.file_handler.read_next().split("  ")[1]
-        self.set_right_side_image(image_path)
-        
-
-    def set_right_side_image(self, image_filename):
-        self.right_image_filename = image_filename
-        if os.path.isfile(self.right_image_filename):
-            image =  Image.open(self.right_image_filename)
-            image.thumbnail((640, 480), Image.ANTIALIAS)
-            self.right_image = ImageTk.PhotoImage(image)
-            self.right_img_label.configure(image =  self.right_image)
-        else:
-            self.right_img_label.configure(image = "", text =  "File Not Found")
-
-        self.right_path_label.configure (text = self.right_image_filename)
+      
+    def next_image(self):
+        logging.debug("next_image")
+        line = self.file_handler.read_next()
+        while line and not (self.left_set and self.right_set):
+            if line == "=====":
+                logging.debug("reset")
+                self.left_set = False
+                self.right_set = False
+            else:
+                image_path = line.split("  ")[1]
+                if os.path.isfile(image_path):
+                    if not self.left_set:
+                        self.set_left_side_image(image_path)
+                    else:
+                        self.set_right_side_image(image_path)
+            if not self.right_set:
+                line = self.file_handler.read_next()
+                        
+        if not (self.left_set and self.right_set):
+            self.status_label.configure (text = "End of duplicates")
+            self.del_left_btn.configure (state = Tkinter.DISABLED)
+            self.keep_both_btn.configure (state = Tkinter.DISABLED)
+            self.del_right_btn.configure (state = Tkinter.DISABLED)
 
     def set_left_side_image(self, image_filename):
+        logging.debug("set_left_file %s", image_filename)
         self.left_image_filename = image_filename
-        if os.path.isfile(self.left_image_filename):
-            image =  Image.open(self.left_image_filename)
-            image.thumbnail((640, 480), Image.ANTIALIAS)
-            self.left_image = ImageTk.PhotoImage(image)
-            self.left_img_label.configure(image =  self.left_image)
-        else:
-            self.left_img_label.configure(image = "", text =  "File Not Found")
-
+        self.left_set = True
+        image =  Image.open(self.left_image_filename)
+        image.thumbnail((640, 480), Image.ANTIALIAS)
+        self.left_image = ImageTk.PhotoImage(image)
+        self.left_img_label.configure(image =  self.left_image)
         self.left_path_label.configure (text = self.left_image_filename)
+
+    def set_right_side_image(self, image_filename):
+        logging.debug("set_right_file %s", image_filename)
+        self.right_image_filename = image_filename
+        self.right_set = True
+        image =  Image.open(self.right_image_filename)
+        image.thumbnail((640, 480), Image.ANTIALIAS)
+        self.right_image = ImageTk.PhotoImage(image)
+        self.right_img_label.configure(image =  self.right_image)
+        self.right_path_label.configure (text = self.right_image_filename)
 
     def keep_both(self):
         self.status_label.configure (text = "Both images kept")
+        self.right_set = False
         self.next_image()
 
     def del_left_picture(self):
         self.file_handler.del_file(self.left_image_filename)
         self.status_label.configure (text = "Deleted:" + self.left_image_filename)
+        self.set_left_side_image(self.right_image_filename)
+        self.right_set = False
         self.next_image()
 
     def del_right_picture(self):
         self.file_handler.del_file(self.right_image_filename)
         self.status_label.configure (text = "Deleted:" + self.right_image_filename)
+        self.right_set = False
         self.next_image()
-
-    def next_image(self):
-        line = self.file_handler.read_next()
-        if line:
-            if line == "=====":
-                self.initialize_images()
-            else:
-                image_path = line.split("  ")[1]
-                self.set_right_side_image(image_path)
-        else:
-            self.status_label.configure (text = "End of duplicates")
-            self.del_left_btn.configure (state = Tkinter.DISABLED)
-            self.keep_both_btn.configure (state = Tkinter.DISABLED)
-            self.del_right_btn.configure (state = Tkinter.DISABLED)
 
 class FileHandler():
     dups_file = None
 
     def __init__(self, dups_filename):
         self.dups_file = open(dups_filename)
-        line = self.read_next()
-        if line != "=====":
-            print line
-            print "This is not a duplicates file or there are no duplicates"
-            sys.exit(1)
 
     def read_next(self):
         return self.dups_file.readline().rstrip()
@@ -129,7 +130,7 @@ class FileHandler():
     
 def main():
     try:
-        logging.basicConfig(filename="deduplicate.log", format='%(asctime)s %(message)s', level=logging.INFO)
+        logging.basicConfig(filename="deduplicate.log", format='%(asctime)s %(message)s', level=logging.DEBUG)
         logging.info("Starting")
         root = Tkinter.Tk()
         root.title("Deduplicate images")
