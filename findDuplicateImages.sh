0c70c5f90cc877532a7299727087ae29ca150c3c
rarsa <rarsa@yahoo.com>
2014-01-02 20:16:00 -0500

    modified to use the imagemagick identify

--- findDuplicateImages.sh
+++ findDuplicateImages.sh
@@ -1,8 +1,8 @@
 index=$(tempfile)
 dupmd5sumfile=$(tempfile)
 
-find "${1}" -type f -iname '*.jpg' -exec md5sum {} \; > ${index}
-#time find "${1}" -type f -iname '*.jpg' -exec identify -format "%# %M\n" {} \; > index2.txt 
+#find "${1}" -type f -iname '*.jpg' -exec md5sum {} \; | sort --key=2 --ignore-case > ${index}
+find "${1}" -type f -iname '*.jpg' -exec identify -format "%#  %M" {} \; > ${index} #| sort --key=2 --ignore-case > ${index}
 
 cut -c1-33 ${index} | sort | uniq -c -d | awk '{print $2}' > ${dupmd5sumfile}
 while read line
